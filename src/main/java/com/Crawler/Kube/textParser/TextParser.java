bef3d1002805a63067a6dc4a15e71a9b277b8d1d
printf-Shivam <shivamkrsingh264@gmail.com>
2026-03-26 23:20:08 +0530

    saving the clean text to db

--- src/main/java/com/Crawler/Kube/textParser/TextParser.java
+++ src/main/java/com/Crawler/Kube/textParser/TextParser.java
@@ -4,49 +4,54 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class TextParser {
     private static final String DBurl = "jdbc:sqlite:searchengine.db";
 
-    public void testParser(){
-        String query = "SELECT url, html_content FROM pages LIMIT 3;";
-
-        try(Connection con = DriverManager.getConnection(DBurl);
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query)){
-
-            System.out.println("HTML parsing test");
-
-            while(rs.next()){
-                String url = rs.getString("url");
-                String rawHtml = rs.getString("html_content");
-
-                Document doc = Jsoup.parse(rawHtml);
-
-                String clean = extractCleanText(doc);
-
-                System.out.println("source url : " + url);
-                System.out.println("original html length : " + rawHtml.length());
-                System.out.println("cleaned text length : " + clean.length());
-
-                if(clean.length() > 0){
-                    System.out.println("snippet: " + clean.substring(0, Math.min(clean.length(),200)));
-                } else {
-                    System.out.println("snippet: [EMPTY / LOW QUALITY]");
-                }
-
-                System.out.println("--------------------------------------------------------\n");
+    public void processAndStoreText(){
+        try (Connection con = DriverManager.getConnection(DBurl);
+             Statement stmt = con.createStatement()){
+            try{
+                stmt.execute("ALTER TABLE pages ADD COLUMN clean_text TEXT;");
+                System.out.println("clean text column added to db");
+            }
+            catch (Exception e){
+                System.out.println("column already exists");
             }
 
-        } catch (Exception e) {
-            System.err.println("db read error");
+            System.out.println("text cleaning started");
+
+            //getting pages to clean
+            String query = "SELECT url, html_content FROM pages WHERE clean_text IS NULL;";
+            ResultSet rs = stmt.executeQuery(query);
+
+            //preparing to update
+            String updateQuery = "UPDATE pages SET clean_text = ? WHERE url = ?";
+            PreparedStatement updateStmt = con.prepareStatement(updateQuery);
+
+            int count=0;
+            while(rs.next()){
+                String url = rs.getString("url");
+                String raw = rs.getString("html_content");
+
+                Document doc = Jsoup.parse(raw);
+                String clean = extractCleanText(doc);
+
+                updateStmt.setString(1,clean);
+                updateStmt.setString(2,url);
+                updateStmt.executeUpdate();
+
+                count++;
+                if(count%50 == 0)
+                    System.out.println("processed and stored " + count + " pages to db");
+            }
+            System.out.println("finished processing and cleaning");
+        }
+        catch(Exception e){
+            System.err.println("db error" + e.getMessage());
         }
     }
-
 
     private String extractCleanText(Document doc) {
         if (doc == null) return "";
@@ -77,6 +82,6 @@ public class TextParser {
     }
 
     public static void main(String[] args) {
-        new TextParser().testParser();
+        new TextParser().processAndStoreText();
     }
 }
